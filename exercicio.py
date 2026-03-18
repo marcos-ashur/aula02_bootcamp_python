2fbf9ee4fd62b1481145e6bdd5867840c6fc36a0
Marcos Ashur <marcosariel120@gmail.com>
2026-03-18 14:42:38 -0300

    adicionado exercicio 3

--- exercicio.py
+++ exercicio.py
@@ -1,4 +1,1 @@
-numero_1 = int(input('digite o número: '))
-numero_2 = int(input('digite o número: '))
-
-print(numero_1 // numero_2)
+print(f"{float(input('digite o número: ').replace(',','.')) * float(input('digite o número: ').replace(',','.')):.2f}")
