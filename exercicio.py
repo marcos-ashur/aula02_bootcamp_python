6b268b69ad6326afc0962da7509a785c5efc2e4d
Marcos Ashur <marcosariel120@gmail.com>
2026-03-18 20:03:11 -0300

    novos exercicios

--- exercicio.py
+++ exercicio.py
@@ -1,23 +1,26 @@
 #print(f"{float(input('digite o número: ').replace(',','.')) * float(input('digite o número: #').replace(',','.')):.2f}")
 
-data = input('Digite a data (dd/mm/aaaa)')
+# data = input('Digite a data (dd/mm/aaaa)')
 
-dia, mes, ano = data.split("/")
+# dia, mes, ano = data.split("/")
 
-print(f'Dia: {dia}')
-print(f'Mês {mes}')
-print(f'Ano {ano}')
+# print(f'Dia: {dia}')
+# print(f'Mês {mes}')
+# print(f'Ano {ano}')
 
-print(10 / 0) 
+# print(10 / 0) 
 
-print(f"Média: {(float(input('digite o núero: ').replace(',','.')) + float(input('digite o número').replace(',','.'))) / 2:.2f}")
+# print(f"Média: {(float(input('digite o núero: ').replace(',','.')) + float(input('digite o número').replace(',','.'))) / 2:.2f}")
 
-numero1 = float(input('digite o número: ').replace(',','.'))
-numero2 = float(input('digite o número: ').replace(',','.'))
-calculo = (numero1 + numero2) / 2
+# numero1 = float(input('digite o número: ').replace(',','.'))
+# numero2 = float(input('digite o número: ').replace(',','.'))
 
-print(calculo)
+# calculo = (numero1 + numero2) / 2
+
+# print(calculo)
 
 
 
+
+# Antes de processar os dados dos usuários em um sistema de recomendação, você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha fornecido um e-mail válido. Escreva um programa que valide essas condições e imprima "Dados de usuário válidos" ou o erro específico encontrado.
 
