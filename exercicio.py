3710e073da00d7e8dc99e10c50870f85f3165663
Marcos Ashur <marcosariel120@gmail.com>
2026-03-18 15:54:17 -0300

    exercício 7

--- exercicio.py
+++ exercicio.py
@@ -1,1 +1,23 @@
-print(f"{float(input('digite o número: ').replace(',','.')) * float(input('digite o número: ').replace(',','.')):.2f}")
+#print(f"{float(input('digite o número: ').replace(',','.')) * float(input('digite o número: #').replace(',','.')):.2f}")
+
+data = input('Digite a data (dd/mm/aaaa)')
+
+dia, mes, ano = data.split("/")
+
+print(f'Dia: {dia}')
+print(f'Mês {mes}')
+print(f'Ano {ano}')
+
+print(10 / 0) 
+
+print(f"Média: {(float(input('digite o núero: ').replace(',','.')) + float(input('digite o número').replace(',','.'))) / 2:.2f}")
+
+numero1 = float(input('digite o número: ').replace(',','.'))
+numero2 = float(input('digite o número: ').replace(',','.'))
+calculo = (numero1 + numero2) / 2
+
+print(calculo)
+
+
+
+
